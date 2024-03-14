ae114559fc33ea69eaff679a9d2215bd7d8ef8ca
AsithandileNyjah <atyuluu@gmail.com>
2024-03-14 14:38:42 +0000

    Backend teaks

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -26,11 +26,7 @@ router.route('/blogs/:blogID').patch(blogEdit);
 router.route('/blogs/:blogID').delete(blogDel);
 
 // login
-router.route('/login').post(valFun,(req,res)=>{
-    res.send({
-        msg: "YAY! You have logged in."
-    })
-})
+router.route('/login').post(valFun)
 
 // adminRights
 
