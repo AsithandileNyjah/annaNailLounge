420dbd9a88c555ddb7e96595e8cc49fdeed9949b
Asithandile Tyulu <140261777+AsithandileNyjah@users.noreply.github.com>
2024-03-16 10:15:53 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ const app = express();
 const PORT = process.env.PORT 
 
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:8080',
+    credentials: true
+  }));
+
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cookieParser());
