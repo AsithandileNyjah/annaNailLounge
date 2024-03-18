9c5949fba6d15138bfe3d49b105e0f740c25be1e
Asithandile Tyulu <140261777+AsithandileNyjah@users.noreply.github.com>
2024-03-18 11:57:39 +0000

    Update middleware.js

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -16,20 +16,16 @@ const validate = (req, res, next) => {
 };
 
 const authMiddleware = (req, res, next) => {
-    const token = req.cookies.jwt;
-
-    if (!token) {
-        return res.status(401).json({ msg: 'Unauthorized: No token provided' });
-    }
-
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.user;
+        const token = req.cookies.jwt;
+        if (!token) {
+            return res.status(401).json({ msg: 'Unauthorized: No token provided' });
+        }
+        jwt.verify(token, process.env.SECRET_KEY);
         next();
     } catch (error) {
         return res.status(401).json({ msg: 'Unauthorized: Invalid token' });
     }
 };
-
 
 export { validate, authMiddleware };
